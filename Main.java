a6586ceb1c0abe059ff19bc7382c51c18cd34dc4
Tehreema-jilani <tehreemaj@gmail.com>
2025-06-27 06:04:38 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+ package tms.main;
+
+import tms.gui.LoginFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Transport Management System");
+        new LoginFrame().setVisible(true);
+    }
+}
